3d3b9022ac3c3819f46994d7b16821ba230a27da
mrassett <magsrassett@gmail.com>
2018-03-31 13:44:23 -0500

    AJAX calls for NYT

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,2 +1,20 @@
 'use strict';
 
+let NYapiKey = "7d7d81b70c5a4f28b5e538f6012ea8ea"; 
+let movieReviewNYapiURL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?api-key=" + NYapiKey + "&q=";
+let articleSearchapiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + NYapiKey + "&q=";
+$.ajax({
+    url: articleSearchapiURL,
+    method: "GET"
+    }).done(function(result){
+    console.log(result);
+    })
+$.ajax({
+    url: movieReviewNYapiURL,
+    method: "GET"
+    }).done(function(result){
+    console.log(result);
+    })
+    document.addEventListener('keypress', function (e){
+        
+    })
