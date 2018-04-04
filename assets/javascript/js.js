32590c66f4c082e5dbf8c7dec2f17025f6d4eccd
wdarsow <wade.darsow@gmail.com>
2018-04-03 20:14:56 -0500

    initial OMDB API call and associated DOM manipulation complete

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,10 +1,44 @@
 'use strict';
 
-let ajaxUrl = "http://www.omdbapi.com/" + "?i=tt3896198&" + "apikey=4b988a5c";
+// initial variable declarations and assignments
+let movieTitle;
+let apiKey = "4b988a5c";
+let ajaxOmdbUrl;
+let clickCounter = 0;
 
-$("#add").on("click", function (event) {
+// when a movie title is typed into the #item text box and the #add / Find Your Movie button is clicked
+// the function below executes. 
+
+$(document).on("click", "#add", function (event) {
     event.preventDefault();
-    let movieTitle = $("#item").val();
 
-    $("#item").val("");
+    // clear any span tags from previous searches if a user has searched for a movie before in the current browser session    
+    if(clickCounter > 0) {
+        $("span").remove(".movie-info");
+    };
+
+    clickCounter++;
+    
+    // variable declaration and assignments
+    movieTitle = $("#item").val().trim();
+    let ajaxOmdbUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=" + apiKey;
+
+    // Ajax call and function that performs the DOM manipulation
+    $.ajax({
+        url: ajaxOmdbUrl,
+        method: "GET",
+    }).then(function(response) {
+        $("#actors").append(`<span class="movie-info">${response.Actors}</span>`);
+        $("#director").append(`<span class="movie-info">${response.Director}</span>`);
+        $("#genre").append(`<span class="movie-info">${response.Genre}</span>`);
+        $("#lang").append(`<span class="movie-info">${response.Language}</span>`);
+        $("#plot").append(`<span class="movie-info">${response.Plot}</span>`);
+        $("#rating").append(`<span class="movie-info">${response.Ratings[0].Value}</span>`);
+        $("#year").append(`<span class="movie-info">${response.Year}</span>`);
+        $("#runTime").append(`<span class="movie-info">${response.Runtime}</span>`);
+    });
+
+// clear the text box that contains the movie title
+$("#item").val("");
+
 });
