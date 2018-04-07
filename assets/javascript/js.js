228a4fc62af4e0bed868f028ded9c350bb1bf7f1
Joan Marie Verba <verba001@umn.edu>
2018-04-07 11:31:56 -0500

    changes

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,32 +1,19 @@
+
 'use strict';
 
+// initial variable declarations and assignments
 
-let NYapiKey = "7d7d81b70c5a4f28b5e538f6012ea8ea"; 
+let NYapiKey = "7d7d81b70c5a4f28b5e538f6012ea8ea";
 let movieReviewNYapiURL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=transformers&api-key=" + NYapiKey + "&q=";
 let articleSearchapiURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + NYapiKey + "&q=";
 
 
-// initial variable declarations and assignments
+
 let movieTitle;
 let apiKey = "4b988a5c";
 let ajaxOmdbUrl;
 let clickCounter = 0;
-let listItems;
-let listMovieTitle;
 
-// check to see if something is in listItems
-if (localStorage.getItem("listItems") === null) {
-    listItems = [];
-    console.log("listitems1 ", listItems);
-} else {
-    listItems = JSON.parse(localStorage.getItem("listItems"));
-    console.log("listitems2 ", listItems);
-    displayItems();
-};
-
-// display current list
-localStorage.setItem("listItems", JSON.stringify(listItems));
-displayItems();
 
 // when a movie title is typed into the #item text box and the #add / Find Your Movie button is clicked
 // the function below executes. 
@@ -36,7 +23,7 @@ $(document).on("click", "#add", function (event) {
 
 
     // clear any span tags from previous searches if a user has searched for a movie before in the current browser session    
-    if(clickCounter > 0) {
+    if (clickCounter > 0) {
         $("span").remove(".movie-info");
     };
 
@@ -44,82 +31,57 @@ $(document).on("click", "#add", function (event) {
 
     // variable declaration and assignments
     movieTitle = $("#item").val().trim();
-    listMovieTitle = movieTitle;
+
     console.log("movietitle ", movieTitle);
     //input validation
-    if (movieTitle === ""){
-        $("#errormsg").text("need input");
+    if (movieTitle === "") {
+        $("#errormsg").text("Input box blank");
     } else {
 
+        // Ajax call to the NYT and function that performs the DOM manipulation
+        $.ajax({
+            url: movieReviewNYapiURL,
+            method: "GET"
+        }).then(function (result) {
+            console.log(result);
+            $('#review-link').attr('href', result.results[0].link.url);
 
-    $.ajax({
-        url: movieReviewNYapiURL,
-        method: "GET"
-    }).then(function (result) {
-        console.log(result);
-        $('#review-link').attr('href', result.results[0].link.url);
-
-        console.log("link ", result.results[0].link.url);
-
-
-    })
-
-    let ajaxOmdbUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=" + apiKey;
-
-    // Ajax call and function that performs the DOM manipulation
-    $.ajax({
-        url: ajaxOmdbUrl,
-        method: "GET",
-    }).then(function(response) {
-        $("#actors").append(`<span class="movie-info">${response.Actors}</span>`);
-        $("#director").append(`<span class="movie-info">${response.Director}</span>`);
-        $("#genre").append(`<span class="movie-info">${response.Genre}</span>`);
-        $("#lang").append(`<span class="movie-info">${response.Language}</span>`);
-        $("#plot").append(`<span class="movie-info">${response.Plot}</span>`);
-        $("#rating").append(`<span class="movie-info">${response.Ratings[0].Value}</span>`);
-        $("#year").append(`<span class="movie-info">${response.Year}</span>`);
-        $("#runTime").append(`<span class="movie-info">${response.Runtime}</span>`);
-    });
-
-    // clear the text box that contains the movie title
-    $("#item").val("");
-  }
-});
+            console.log("link ", result.results[0].link.url);
 
 
-// add the movie to the list (when button is pressed)
-$(".listbtn").on("click", function (event) {
-    event.preventDefault();
-    console.log("List button clicked");
-    listItems.push(listMovieTitle);
-    localStorage.setItem("listItems", JSON.stringify(listItems));
-    displayItems();
-});
+        })
 
-// run on page load and every time button is clicked
-function displayItems() {
-    $("#movielist").empty();
-    let data = JSON.parse(localStorage.getItem("listItems"));
-    console.log("data ", data);
-    for (let i = 0; i < data.length; i++) {
-        let myDiv = $("<div>");
-        let myButton = $("<button>");
-        myButton.attr("data", i);
-        myButton.append("✓");
-        myButton.addClass("check");
-        myDiv.append(myButton);
-        myDiv.append(" " + data[i]);
-        $("#movielist").append(myDiv);
-        console.log("list output ", data[i]);
+
+        let ajaxOmdbUrl = "http://www.omdbapi.com/?t=" + movieTitle + "&apikey=" + apiKey;
+
+        // Ajax call to the OMDB and function that performs the DOM manipulation
+        $.ajax({
+            url: ajaxOmdbUrl,
+            method: "GET",
+        }).then(function (response) {
+            $("#title").append(`<span class="movie-info">${response.Title}</span>`);
+            $("#actors").append(`<span class="movie-info">${response.Actors}</span>`);
+            $("#director").append(`<span class="movie-info">${response.Director}</span>`);
+            $("#genre").append(`<span class="movie-info">${response.Genre}</span>`);
+            $("#lang").append(`<span class="movie-info">${response.Language}</span>`);
+            $("#plot").append(`<span class="movie-info">${response.Plot}</span>`);
+            $("#rating").append(`<span class="movie-info">${response.Ratings[0].Value}</span>`);
+            $("#year").append(`<span class="movie-info">${response.Year}</span>`);
+            $("#runTime").append(`<span class="movie-info">${response.Runtime}</span>`);
+
+            // Retrieving the URL for the image
+            let imgURL = response.Poster;
+            console.log("response.Poster ", response.Poster);
+            console.log("imgURL ", imgURL);
+            // Creating an element to hold the image
+            let image = $("<img>").attr("src", imgURL);
+            // Appending the image
+            $("#movie-poster").append(image);
+
+        });
+
+
+        // clear the text box that contains the movie title
+        $("#item").val("");
     }
-};
-
-
-//remove item from the list
-$(document).on("click", ".check", function () {
-    let eachItem = $(this).attr("data");
-    listItems.splice(eachItem, 1);
-    localStorage.setItem("listItems", JSON.stringify(listItems));
-    displayItems();
 });
-
